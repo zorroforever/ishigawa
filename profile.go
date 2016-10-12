d698a96259c046904a3a282e68b4d4ea847d91bb
mosen <mosen@users.noreply.github.com>
2016-10-12 12:07:52 +1100

    Hotfix/enroll payloadscope (#31)
    * Stub new method for application datastore
    Handle command failures more gracefully than continuously retrying. Failures are not logged
    Add table for certificates.
    
    (cherry picked from commit 9f580a6)
    
    * Remove methods with signature NewDatastore() from datastores and injection of the logger instance into each datastore because groob is a doodoo head :)
    Move NewDatastore methods into unit tests
    Reinstate NewDB test
    Create certificates package including a Certificate type and a datastore.
    Began working on a method ackCertificateList in the connect service.
    Boilerplate for management service test.
    Renamed some tables in certificates migration, hopefully nobody notices.
    Applications response handler should be omitted from this branch.
    
    (cherry picked from commit 8bc2fc7)
    
    * Add device uuid to certificate, as there will never be a normalised form of the certificate data.
    Change statments to reflect table change from certificates to devices_certificates
    Add the CertificateList request type to the connect service's Acknowledge method
    Add certificates datastore to the management service, and add endpoints and request/responses for retrieving certificates by device uuid to the management endpoujnt.
    Kludge. commits regarding app service will be amended.
    (cherry picked from commit c723c80)
    
    * Don't forget the endpoint definition for certificates!
    
    (cherry picked from commit 4e5fc1f)
    
    * Use byte field for certificate data.
    
    (cherry picked from commit 66d5036)
    
    * Fix empty import in certificates
    
    (cherry picked from commit b6d4dca)
    
    * Add table for certificates.
    
    (cherry picked from commit 06bc231)
    
    * Renamed some tables in certificates migration, hopefully nobody notices.
    Rebase develop onto master
    
    (cherry picked from commit 775484d)
    
    * Certificate list responses are saved via replacing the entire certificate list on a per device basis. This is because neither the common name nor the data could be used as a unique constraint in the certificates table.
    Few small changes to imports/style.
    
    (cherry picked from commit e0a63e1)
    
    * Properly rollback if certificate insert fails for any certificate in a response.
    
    (cherry picked from commit 99e1ea1)
    
    * Fix several incorrect statements and struct tags which were preventing the certificates management endpoint from listing certs.
    Certificate listing is working without a base64 encoded representation of each certificate.
    
    (cherry picked from commit 685dfef)
    
    * Add commands index to command datastore.
    Add simple test for commands index
    Add commands index endpoint
    
    (cherry picked from commit 4e2a2a2)
    
    * Add handler for GET /mdm/commands
    
    (cherry picked from commit ae71b8f)
    
    * Added Find() method to commands datastore so that the request that matches a response can be retrieved by the connect service.
    Added Find() method to command service
    
    (cherry picked from commit ebceb28)
    
    * Fix globally scoped vars in command datastore test suite
    
    * groob prefers inline definition of struct members.
    
    * Uppercase CommandUuid
    
    * Add ackCertificateList to connect service, accidentally omitted from installed_certificate_list PR #14
    Make push endpoint conform to Errorer interface.
    Wrap error return in management service.
    Add missing certsDB in main.go, should have been part of installed_certificate_list PR #14
    
    * Fix erraneous comma in mdmEnrollResponse
    
    * Model is sent with Authenticate message so best to update it there without having to query DeviceInformation.
    
    * Incorrect struct tags result in PayloadScope not being included in the mdm enroll profile output.

--- profile.go
+++ profile.go
@@ -13,7 +13,7 @@ type Payload struct {
 	PayloadDisplayName  string      `json:"displayname" db:"displayname"`
 	PayloadDescription  string      `json:"description,omitempty" db:"description"`
 	PayloadOrganization string      `json:"organization,omitempty" db:"organization"`
-	PayloadScope        string      `json:"scope" db:"scope" plist:"omitempty"`
+	PayloadScope        string      `json:"scope" db:"scope" plist:",omitempty"`
 	PayloadContent      interface{} `json:"content,omitempty" plist:"PayloadContent,omitempty"`
 }
 
@@ -21,16 +21,16 @@ type Profile struct {
 	PayloadContent           []interface{}     `json:"content,omitempty" db:"content"`
 	PayloadDescription       string            `json:"description,omitempty" db:"description"`
 	PayloadDisplayName       string            `json:"displayname,omitempty" db:"displayname"`
-	PayloadExpirationDate    *time.Time        `json:"expiration_date,omitempty" db:"expiration_date" plist:"omitempty"`
+	PayloadExpirationDate    *time.Time        `json:"expiration_date,omitempty" db:"expiration_date" plist:",omitempty"`
 	PayloadIdentifier        string            `json:"identifier" db:"identifier"`
 	PayloadOrganization      string            `json:"organization,omitempty" db:"organization"`
 	PayloadUUID              string            `json:"uuid" db:"uuid"`
-	PayloadRemovalDisallowed bool              `json:"removal_disallowed" db:"removal_disallowed" plist:"omitempty"`
+	PayloadRemovalDisallowed bool              `json:"removal_disallowed" db:"removal_disallowed" plist:",omitempty"`
 	PayloadType              string            `json:"type" db:"type"`
 	PayloadVersion           int               `json:"version" db:"version"`
-	PayloadScope             string            `json:"scope" db:"scope" plist:"omitempty"`
-	RemovalDate              *time.Time        `json:"removal_date" db:"removal_date" plist:"-" plist:"omitempty"`
-	DurationUntilRemoval     float32           `json:"duration_until_removal" db:"duration_until_removal" plist:"omitempty"`
+	PayloadScope             string            `json:"scope" db:"scope" plist:",omitempty"`
+	RemovalDate              *time.Time        `json:"removal_date" db:"removal_date" plist:"-" plist:",omitempty"`
+	DurationUntilRemoval     float32           `json:"duration_until_removal" db:"duration_until_removal" plist:",omitempty"`
 	ConsentText              map[string]string `json:"consent_text" db:"consent_text" plist:"omitempty"`
 }
 
