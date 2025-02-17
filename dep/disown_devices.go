12de630a1df2449ecbbd86ec5757a3fcf391f3a2
liuwanjie <liuwanjie@geexfinance.com>
2025-02-17 13:56:08 +0800

    remove net/url

--- dep/disown_devices.go
+++ dep/disown_devices.go
@@ -2,22 +2,19 @@ package dep
 
 import (
 	"github.com/pkg/errors"
-	"net/url"
 )
 
 const (
-	disownDevicesPath        = "device/disown"
+	disownDevicesPath = "device/disown"
 )
 
 type DisownDevicesRequest struct {
 	Device []string `json:"device"`
-
 }
 
 type DisownDevicesResponse struct {
 	Status string `json:"response_status"`
 }
-
 
 func (c *Client) DisownDevices(alr *DisownDevicesRequest) (*DisownDevicesResponse, error) {
 	req, err := c.newRequest("POST", disownDevicesPath, &alr)
@@ -28,4 +25,3 @@ func (c *Client) DisownDevices(alr *DisownDevicesRequest) (*DisownDevicesRespons
 	err = c.do(req, &response)
 	return &response, errors.Wrap(err, "disown device")
 }
-
